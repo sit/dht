f83b4e71d32fe2db4fb961d7dc32be2d0987d0ed
Frank Dabek <fdabek@csail.mit.edu>
2001-02-05 17:14:45 +0000

    testing for 033 page

--- sfsrosd/client.C
+++ sfsrosd/client.C
@@ -1,3 +1,10 @@
+// $Date: 2001/02/05 17:16:10 $
+
+// $Date
+
+// $Author
+
+// last modified by $Author: fdabek $ on $Date: 2001/02/05 17:16:10 $
 #include <sfsrosd.h>
 #include "sfsdb.h"
 #include "rxx.h"
@@ -14,8 +21,11 @@ sfsrodb db;
 sfs_connectres cres;
 sfs_fsinfo fsinfores;
 extern vec<sfsro_mirrorarg> mirrors;
-//qhash<sfs_hash, ref<sfsro_datares> > cache;
 
+#ifdef STATS
+int proxy_requests;
+int proxy_misses;
+#endif /* #ifdef STATS */
 
 sfsroclient::sfsroclient (ptr<axprt_stream> _x, const authunix_parms *aup)
   : x (_x), destroyed (new refcounted<bool> (false))
@@ -24,24 +34,30 @@ sfsroclient::sfsroclient (ptr<axprt_stream> _x, const authunix_parms *aup)
 
   if ((unixauth = aup))
     uid = aup->aup_uid;
-
+  
   rosrv = asrv::alloc (x, sfsro_program_1,
-			 wrap (this, &sfsroclient::dispatch, destroyed));
+		       wrap (this, &sfsroclient::dispatch, destroyed));
   sfssrv = asrv::alloc (x, sfs_program_1,
 			wrap (this, &sfsroclient::dispatch, destroyed));
   authid_valid = false;
 
+#ifdef STATS
+  proxy_requests = 0;
+  proxy_misses = 0;
+#endif
 }
 
 void
-sfsroclient::updatemirrorinfo() {
+sfsroclient::updatemirrorinfo() 
+{
   
   //FED - update fsinfo
   fsinfores.sfsro->v1->mirrors.set (mirrors.base(), mirrors.size (), freemode::NOFREE); 
 }
 
 void
-sfsroclient::addmirror(sfsro_mirrorarg *mirror) {
+sfsroclient::addmirror(sfsro_mirrorarg *mirror) 
+{
 
   //FED - update global mirror list
   mirrors.push_back(*mirror);
@@ -51,6 +67,11 @@ sfsroclient::addmirror(sfsro_mirrorarg *mirror) {
 sfsroclient::~sfsroclient()
 {
   *destroyed = true;
+
+#ifdef STATS
+  warn << "total proxy requests: " << proxy_requests << "\n";
+  warn << "proxy misses: " << proxy_misses << "\n";
+#endif
 }
 
 void
@@ -109,20 +130,30 @@ sfsroclient::dispatch (ref<bool> d, svccb *sbp)
 
     case SFSROPROC_PROXYGETDATA:
       {
-	warn << "got a proxy data request\n";
 	sfsro_proxygetarg *arg = sbp->template getarg<sfsro_proxygetarg> ();
-	
+
 	struct in_addr pubaddr;
 	pubaddr.s_addr = arg->pub_addr;
-	char buf[100];
-	warn << "I think that the publisher is.... ";
-	warn << inet_ntop(AF_INET, &pubaddr, buf, 100) << "\n";
-	//this is where I would check the cache
-	//right here
 
 	sfsro_datares *res = New sfsro_datares();
 	ref<sfs_hash> fh = new refcounted<sfs_hash>(arg->fh);
 	db.getdata(fh, res, wrap(this, &sfsroclient::proxygetdata_cb, sbp, res, destroyed));
+	return;
+	  
+      }
+
+    case SFSROPROC_PROXYGETDATA_PARTIAL:
+      {
+	sfsro_partialproxygetarg *arg = sbp->template getarg<sfsro_partialproxygetarg> ();
+	
+	struct in_addr pubaddr;
+	pubaddr.s_addr = arg->pub_addr;
+
+	sfsro_datares *res = New sfsro_datares();
+	ref<sfs_hash> fh = new refcounted<sfs_hash>(arg->fh);
+	db.getpartialdata(fh, res, 
+			  wrap(this, &sfsroclient::partialproxygetdata_cb, sbp, res, destroyed),
+			  arg->offset, arg->len);
 	return;
 	  
       }
@@ -149,8 +180,6 @@ sfsroclient::dispatch (ref<bool> d, svccb *sbp)
 	  printf (" },\n");
 	}
 
-	//warn << "got a partial get request for " << arg->offset << " to " << arg->len+arg->offset << "\n";
-	
 	sfsro_datares *res = New sfsro_datares();
         db.getpartialdata(&(arg->key), res, 
 			  wrap(this, &sfsroclient::getdata_cb, 
@@ -174,7 +203,50 @@ sfsroclient::dispatch (ref<bool> d, svccb *sbp)
 }
 
 void
-sfsroclient::getdata_cb(svccb *sbp, sfsro_datares *res, ref<bool> d) {
+sfsroclient::remote_get_data(sfs_hash fh,
+			     struct in_addr *pub_addr, 
+			     short pub_port,
+			     sfsro_datares *res,
+			     callback<void, sfsro_datares *>::ref cb) 
+{
+  tcpconnect(*pub_addr, pub_port, 
+	     wrap(this, &sfsroclient::remoteget_cb, fh, cb, res));
+};
+
+void
+sfsroclient::remoteget_cb(sfs_hash fh,
+			  callback<void, sfsro_datares *>::ref cb,
+			  sfsro_datares *res,
+			  int fd) 
+{
+  
+  ptr<axprt_stream> x = axprt_stream::alloc (fd);
+  ptr<aclnt> sfsroc = aclnt::alloc (x, sfsro_program_1);
+  
+  ref<sfs_hash> rfh = New refcounted<sfs_hash>(fh);
+  sfsroc->call (SFSROPROC_GETDATA, rfh, res, 
+		wrap(this, &sfsroclient::remotegetdata_cb_call, fh, cb, res));
+}
+
+void
+sfsroclient::remotegetdata_cb_call(sfs_hash fh, 
+		      callback<void, sfsro_datares *>::ref cb,
+		      sfsro_datares *res,
+		      clnt_stat err) 
+{
+
+  if (err) {
+    warn << "error fetching data remotely\n";
+    (*cb)(NULL);
+    return;
+  }
+
+  (*cb)(res);
+}
+
+void
+sfsroclient::getdata_cb(svccb *sbp, sfsro_datares *res, ref<bool> d) 
+{
   if (*d)
     return;
   sbp->reply(res);
@@ -183,47 +255,102 @@ sfsroclient::getdata_cb(svccb *sbp, sfsro_datares *res, ref<bool> d) {
 }
 
 void
-sfsroclient::proxygetdata_cb(svccb *sbp, sfsro_datares *res, ref<bool> d) {
+sfsroclient::proxygetdata_cb(svccb *sbp, sfsro_datares *res, ref<bool> d) 
+{
   if (*d)
     return;
+
+#ifdef STATS
+  proxy_requests++;
+#endif
 
   if (res->status == SFSRO_OK) {
     sbp->reply(res);
     delete (res);
     return;
   } else {
-    warn << "cache miss, contacting publisher\n";
-    //contact the publisher
+#ifdef STATS
+    proxy_misses++;
+#endif
     sfsro_proxygetarg *arg = sbp->template getarg<sfsro_proxygetarg> ();
     struct in_addr pubaddr;
     pubaddr.s_addr = arg->pub_addr;
-    tcpconnect(pubaddr, arg->pub_port, 
-	       wrap(this, &sfsroclient::proxygetdata_cb_connect, sbp, res));
+    remote_get_data(arg->fh, &pubaddr, arg->pub_port, res,
+		    wrap(this, &sfsroclient::proxygetdata_cb_call, sbp));
     return;
   }
   
 }
 
 void
-sfsroclient::proxygetdata_cb_connect(svccb *sbp, sfsro_datares *res , int fd) {
+sfsroclient::proxygetdata_cb_call(svccb *sbp, sfsro_datares *res) 
+{
 
-  ptr<axprt_stream> x = axprt_stream::alloc (fd);
-  ptr<aclnt> sfsroc = aclnt::alloc (x, sfsro_program_1);
-
+  if (res == NULL) {
+    sbp->reply(NULL);
+    return;
+  }
+  
   sfsro_proxygetarg *arg = sbp->template getarg<sfsro_proxygetarg> ();
   ref<sfs_hash> fh = New refcounted<sfs_hash>(arg->fh);
-  sfsroc->call (SFSROPROC_GETDATA, fh, res, wrap(this,
-						 &sfsroclient::proxygetdata_cb_call, sbp, res));
+  db.putdata(fh, res);
+
+  sbp->reply(res);
 }
 
 void
-sfsroclient::proxygetdata_cb_call(svccb *sbp, sfsro_datares *res, clnt_stat err) {
+sfsroclient::partialproxygetdata_cb(svccb *sbp, sfsro_datares *res, ref<bool> d) 
+{
+  if (*d)
+    return;
 
-  if (err) {
-    fatal << "Error contacting publisher\n";
+#ifdef STATS
+  proxy_requests++;
+#endif
+
+  if (res->status == SFSRO_OK) {
+    sbp->reply(res);
+    delete (res);
+    return;
+  } else {
+#ifdef STATS
+    proxy_misses++;
+#endif
+    sfsro_proxygetarg *arg = sbp->template getarg<sfsro_proxygetarg> ();
+    struct in_addr pubaddr;
+    pubaddr.s_addr = arg->pub_addr;
+    remote_get_data(arg->fh, &pubaddr, arg->pub_port, res, 
+		   wrap(this, &sfsroclient::partialproxygetdata_cb_call, sbp) );
+    return;
   }
   
-  sbp->reply(res);
+}
+
+void
+sfsroclient::partialproxygetdata_cb_call(svccb *sbp, sfsro_datares *res) {
+
+  if (res == NULL) {
+    sbp->reply(NULL);
+    return;
+  }
+  
+  sfsro_partialproxygetarg *arg = sbp->template getarg<sfsro_partialproxygetarg> ();
+  ref<sfs_hash> fh = New refcounted<sfs_hash>(arg->fh);
+  db.putdata(fh, res);
+
+  //res is a full data block, we must segment it here
+  sfsro_datares *split_res = New sfsro_datares();
+  int reslen = res->resok->data.size();
+  int fraclen = (reslen/64);
+  int len = (fraclen)*(arg->len);
+  if (arg->offset + arg->len == 64) len += (reslen % 64);
+  int start = (fraclen)*(arg->offset);
+
+  split_res->set_status(SFSRO_OK);
+  split_res->resok->data.setsize(len);
+  memcpy(split_res->resok->data.base(), (void *)((char *)res->resok->data.base() + start), len);
+
+  sbp->reply(split_res);
 }
 
 
