a5443b805fcdd6d8fbdfc547bbc428212b706613
Frank Dabek <fdabek@csail.mit.edu>
2001-11-07 23:10:35 +0000

    lisence stuff

--- dhash/server.C
+++ dhash/server.C
@@ -1,3 +1,31 @@
+/*
+ *
+ * Copyright (C) 2001  Frank Dabek (fdabek@lcs.mit.edu), 
+ *                     Frans Kaashoek (kaashoek@lcs.mit.edu),
+ *   		       Massachusetts Institute of Technology
+ * 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 #include <dhash.h>
 #include <dhash_prot.h>
 #include <chord.h>
@@ -5,8 +33,9 @@
 #include <chord_util.h>
 #include <dbfe.h>
 #include <arpc.h>
+#ifdef DMALLOC
 #include <dmalloc.h>
-
+#endif
 
 dhash::dhash(str dbname, vnode *node, int k, int ss, int cs, int _ss_mode) :
   ss_mode (_ss_mode / 10), host_node (node), key_store(ss), key_cache(cs) {
