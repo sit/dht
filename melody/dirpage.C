cafc0ebb5182e3a5a88305bcd55347332e5d7ebe
James Robertson <jsr@pdos.csail.mit.edu>
2002-05-15 19:46:51 +0000

    another fix for ycheng. gotta test linux builds right away

--- melody/dirpage.C
+++ melody/dirpage.C
@@ -135,14 +135,13 @@ dirpage::listdir(str path) {
 void
 dirpage::fileout_head (int asize)
 {
-  char *foo;
+  char foo[1024]; // ridiculusly large
   size = asize;
-  asprintf(&foo, "%d\r\n\r\n", size); // FIXME return == null?
+  sprintf(&foo, "%d\r\n\r\n", size); // FIXME return == null?
 warn << (int)d->cs << " fileout_head size " << size << "\n";
   out->take(dphead);
   out->take(dpfile);
   out->take(foo);
-  free(foo);
   cc->next();
 }
 
