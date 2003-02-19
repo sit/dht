f43cf995fcff731b2c244186c198c8d4e9447a5d
Josh Cates <cates@mit.edu>
2003-02-19 19:53:27 +0000

    new utilities to help collect statistics. walk -- walks around the ring printing out each node builddb -- creates and populates a database with random blocks           (compared to running dbm: this saves a lot of time and bandwidth)

---
+++ devel/builddb.C
@@ -0,0 +1,96 @@
+#include "async.h"
+#include "chord.h"
+#include "dhash.h"
+#include "dbfe.h"
+
+#include "rxx.h"
+#include "async.h"
+
+char *dbname;
+chordID minID, maxID;
+u_int block_size;
+u_int num_blocks;
+
+void 
+usage ()
+{
+  fatal << "USAGE: builddb <dbname> <minID> <maxID> <bytes/block> <#blocks>\n";
+}
+
+
+ptr<dbfe>
+opendb()
+{
+  unlink (dbname);
+
+  dbOptions opts;
+  opts.addOption("opt_async", 1);
+  opts.addOption("opt_cachesize", 1000);
+  opts.addOption("opt_nodesize", 4096);
+
+  ptr<dbfe> db = New refcounted<dbfe>();
+  if (int err = db->opendb(dbname, opts)) {
+    warn << "db file: " << dbname <<"\n";
+    warn << "open returned: " << strerror(err) << "\n";
+    exit (-1);
+  }
+
+  return db;
+}
+
+
+int
+main (int argc, char** argv) 
+{
+  setprogname (argv[0]);
+  sfsconst_init ();
+  random_init ();
+
+  if (argc != 6
+      || !str2chordID (argv[2], minID)
+      || !str2chordID (argv[3], maxID)
+      || !(block_size = atoi (argv[4]))
+      || !(num_blocks = atoi (argv[5])))
+    usage ();
+
+  dbname = argv[1];
+
+  warn << "PARAMS dbname '" << dbname 
+       << "', minID " << minID
+       << ", maxID " << maxID
+       << ", bytes/block " << block_size
+       << ", #blocks " << num_blocks
+       << "\n";
+
+
+  ptr<dbfe> db = opendb ();
+
+
+  char block[block_size];
+  bzero (block, block_size);
+  u_int n = 0;
+  for (u_int i = 0; i < num_blocks; i++) {
+    // start of block contains 'i', the rest is all zeroed
+    ((u_int *)block)[0] = i; 
+
+    char hash[sha1::hashsize];
+    sha1_hash (hash, block, block_size);
+    chordID ID;
+    mpz_set_rawmag_be (&ID, hash, sizeof (hash));
+
+    if (between (minID, maxID, ID)) {
+      n++;
+      ref<dbrec> key = New refcounted<dbrec> (&hash[0], sha1::hashsize);
+      ref<dbrec> data = New refcounted<dbrec> (&block[0], block_size);
+      db->insert (key, data);
+      warn << n << "\n";
+    } 
+  }
+
+  db->sync ();
+
+  warn << n << "/" << num_blocks << " blocks inserted in " << dbname << ".\n";
+}
+
+
+
---
+++ devel/walk.C
@@ -0,0 +1,151 @@
+#include "chord.h"
+#include "math.h"
+#include "rxx.h"
+#include "async.h"
+
+#define TIMEOUT 10
+
+chordID wellknown_ID = -1;
+ptr<axprt_dgram> dgram_xprt;
+
+void getsucc_cb (chord_nodelistextres *res, clnt_stat err);
+
+void
+setup () 
+{
+  int dgram_fd = inetsocket (SOCK_DGRAM);
+  if (dgram_fd < 0) fatal << "Failed to allocate dgram socket\n";
+  dgram_xprt = axprt_dgram::alloc (dgram_fd, sizeof(sockaddr), 230000);
+  if (!dgram_xprt) fatal << "Failed to allocate dgram xprt\n";
+}
+
+ptr<aclnt>
+get_aclnt (str host, unsigned short port)
+{
+  sockaddr_in saddr;
+  bzero(&saddr, sizeof(sockaddr_in));
+  saddr.sin_family = AF_INET;
+  inet_aton (host.cstr (), &saddr.sin_addr);
+  saddr.sin_port = htons (port);
+
+  ptr<aclnt> c = aclnt::alloc (dgram_xprt, chord_program_1, 
+			       (sockaddr *)&(saddr));
+
+  return c;
+}
+
+void
+doRPC (str host, u_short port, int procno, const void *in, void *out, aclnt_cb cb)
+{
+  ptr<aclnt> c = get_aclnt (host, port);
+  if (c == NULL) 
+    fatal << "doRPC: couldn't aclnt::alloc\n";
+  c->timedcall (TIMEOUT, procno, in, out, cb);
+}  
+
+
+void
+getsucc (chordID n, str host, u_short port)
+{
+  chord_nodelistextres *res = New chord_nodelistextres ();
+  doRPC (host, port, CHORDPROC_GETSUCC_EXT, &n, res,
+	 wrap (&getsucc_cb, res));
+}
+
+
+int
+is_authenticID (const chordID &x, str n, int p)
+{
+  chordID ID;
+  char id[sha1::hashsize];
+  
+  // xxx presumably there's really a smaller actual range
+  //     of valid ports.
+  if (p < 0 || p > 65535)
+    return -1;
+  
+  for (int i = 0; i < chord::max_vnodes; i++) {
+    // XXX i bet there's a faster way to construct these
+    //     string objects.
+    str ids = n << "." << p << "." << i;
+    sha1_hash (id, ids, ids.len ());
+    mpz_set_rawmag_be (&ID, id, sizeof (id));  // For big endian    
+
+    if (ID == x) return i;
+  }
+  return -1;
+}
+
+void
+getsucc_cb (chord_nodelistextres *res, clnt_stat err)
+{
+  assert (err == 0 && res->status == CHORD_OK);
+  assert (res->resok->nlist.size () >= 2);
+
+  chordID n    = res->resok->nlist[1].x;
+  str host     = res->resok->nlist[1].r.hostname;
+  u_short port = res->resok->nlist[1].r.port;
+  int index    = is_authenticID (n, host, port);
+  warnx << n << " " << host << " " << " " << index << "\n";
+
+  // wrapped around ring. done.
+  if (n == wellknown_ID)
+    exit (0);
+
+  getsucc (n, host, port);
+}
+
+void 
+usage ()
+{
+  fatal << "walk -j <host>:<port>\n";
+}
+
+int
+main (int argc, char** argv) 
+{
+  setprogname (argv[0]);
+  sfsconst_init ();
+  random_init ();
+  setup ();
+
+  str host = "not set";
+  unsigned short port = 0;
+
+  int ch;
+  while ((ch = getopt (argc, argv, "h:j:a:l:f:is:")) != -1) {
+    switch (ch) {
+    case 'j': 
+      {
+	char *bs_port = strchr(optarg, ':');
+	if (!bs_port) usage ();
+	*bs_port = 0;
+	bs_port++;
+	if (inet_addr (optarg) == INADDR_NONE) {
+	  //yep, this blocks
+	  struct hostent *h = gethostbyname (optarg);
+	  if (!h) {
+	    warn << "Invalid address or hostname: " << optarg << "\n";
+	    usage ();
+	  }
+	  struct in_addr *ptr = (struct in_addr *)h->h_addr;
+	  host = inet_ntoa (*ptr);
+	} else
+	  host = optarg;
+
+	port = atoi (bs_port);
+
+	break;
+      }
+    };
+  }
+
+  if (host == "not set")
+    usage ();
+    
+  wellknown_ID = make_chordID (host, port, 0);
+  getsucc (wellknown_ID, host, port);
+
+
+  amain ();
+}
