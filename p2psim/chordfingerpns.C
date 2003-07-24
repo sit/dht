0438a259b3b7f2824e2e8bbf1cf4ed129ebd35dd
Jinyang Li <jinyang@csail.mit.edu>
2003-07-24 21:21:03 +0000

    forgot to add files..

---
+++ p2psim/chordfingerpns.C
@@ -0,0 +1,101 @@
+#include  "chordfingerpns.h"
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include "network.h"
+#include "topology.h"
+
+using namespace std;
+
+
+ChordFingerPNS::ChordFingerPNS(Node *n, uint base, uint successors, uint samples) : Chord(n, successors), _base(base), _samples(samples)
+{
+  srandom(1);
+}
+
+void
+ChordFingerPNS::init_state(vector<IDMap> ids)
+{
+  loctable->set_evict(false);
+
+  uint sz = ids.size();
+  uint my_pos = find(ids.begin(), ids.end(), me) - ids.begin();
+  assert(ids[my_pos].id == me.id);
+  CHID min_lap = ids[(my_pos+1) % sz].id - me.id;
+
+  CHID lap = (CHID) -1;
+
+  assert(_base == 2);
+
+  Topology *t = Network::Instance()->gettopology();
+  IDMap tmpf;
+  while (lap > min_lap) {
+    lap = lap / _base;
+    for (uint j = 1; j <= (_base -1); j++) {
+      if (lap * j < min_lap) continue;
+      tmpf.id = lap * j + me.id;
+      uint s_pos = upper_bound(ids.begin(), ids.end(), tmpf, Chord::IDMap::cmp) - ids.begin();
+      s_pos = s_pos % sz;
+      tmpf.id = lap * (j+1) + me.id;
+      uint e_pos = upper_bound(ids.begin(), ids.end(), tmpf, Chord::IDMap::cmp) - ids.begin();
+      e_pos = e_pos % sz;
+      uint candidates = (e_pos - s_pos) % sz;
+      double prob = (double)_samples/(double)(candidates);
+      IDMap min_f = me;
+      uint min_l = 10000000;
+      if (candidates > 10 * _samples) {
+	//use a more efficient sampling technique
+	for (uint i = 0; i < _samples; i++) {
+	  i = uint (((double)random()/(double)RAND_MAX) * candidates);
+	  if (t->latency(me.ip, ids[(s_pos + i) % sz].ip) < min_l) {
+	    min_f = ids[(s_pos + i) % sz];
+	    min_l = t->latency(me.ip, ids[(s_pos + i)%sz].ip);
+	  }
+	}
+      }else {
+	for (uint i = s_pos; i!= e_pos; i = (i+1)%sz) {
+	  double r = (double) random()/(double)RAND_MAX;
+	  if (r < prob) { //sample only _samples out of all candidates approximately
+	    if (t->latency(me.ip,ids[i].ip) < min_l) {
+	      min_f = ids[i];
+	      min_l = t->latency(me.ip, ids[i].ip);
+	    }
+	  }
+	}
+      }
+      loctable->add_node(min_f);
+    }
+  }
+
+  //add successors
+  Chord::init_state(ids);
+}
+
+bool
+ChordFingerPNS::stabilized(vector<CHID> lid)
+{
+  return true;
+}
+
+void
+ChordFingerPNS::dump()
+{
+  Chord::dump();
+  IDMap succ = loctable->succ(me.id + 1);
+  CHID min_lap = succ.id - me.id;
+  CHID lap = (CHID) -1;
+  CHID finger;
+
+  Topology *t = Network::Instance()->gettopology();
+
+  while (lap > min_lap) {
+    lap = lap / _base;
+    for (uint j = 1; j <= (_base - 1); j++) {
+      if ((lap * j) < min_lap) continue;
+      finger = lap * j + me.id;
+      succ = loctable->succ(finger);
+      if (succ.ip > 0) 
+        printf("%qx: finger: %qx,%d : %qx : succ %qx lat %d\n", me.id, lap, j, finger, succ.id, t->latency(me.ip, succ.ip));
+    }
+  }
+}
---
+++ p2psim/chordfingerpns.h
@@ -0,0 +1,23 @@
+#ifndef __CHORDFINGERPNS_H
+#define __CHORDFINGERPNS_H
+
+#include "chord.h"
+
+/* ChordFingerPNS does Gummadi^2's PNS proximity routing, it's completely static now*/
+
+class ChordFingerPNS: public Chord {
+  public:
+    ChordFingerPNS(Node *n, uint base, uint successors, uint samples = 16);
+    ~ChordFingerPNS() {};
+    string proto_name() { return "ChordFingerPNS"; }
+
+    bool stabilized(vector<CHID> lid);
+    void dump();
+    void init_state(vector<IDMap> ids);
+
+  protected:
+    uint _base;
+    uint _samples;
+};
+
+#endif
