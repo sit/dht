f9971fad38e879663a6f79385e6c9f8694fd0633
Thomer Gil <thomer@pdos.csail.mit.edu>
2003-10-08 07:10:38 +0000

    copyright notice in preparation of a release at some point

--- p2psim/protocols/tapestry.h
+++ p2psim/protocols/tapestry.h
@@ -1,4 +1,27 @@
-/* $Id: tapestry.h,v 1.1 2003/10/08 06:43:00 thomer Exp $ */
+/*
+ * Copyright (c) 2003 [NAMES_GO_HERE]
+ *                    Massachusetts Institute of Technology
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ */
+
+/* $Id: tapestry.h,v 1.2 2003/10/08 07:10:40 thomer Exp $ */
 
 #ifndef __TAPESTRY_H
 #define __TAPESTRY_H
