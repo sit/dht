c5472d99c3274a2ee7446348530589ba7effbf28
Robert Morris <rtm@csail.mit.edu>
2003-10-24 01:49:29 +0000

    Gossip more properly.

--- p2psim/protocols/kelips.C
+++ p2psim/protocols/kelips.C
@@ -35,6 +35,8 @@ Kelips::Kelips(Node *n, Args a)
 {
   _k = a.nget<unsigned>("k", 20, 10);
   assert(_k > 0);
+  _rounds = 0;
+  _stable = false;
 }
 
 string
@@ -47,28 +49,12 @@ i2s(unsigned long long x)
 
 Kelips::~Kelips()
 {
-  check();
-#if 0
-  string s;
-  s = i2s(ip());
-  s += "/";
-  s += i2s(group());
-  s += ": ";
-  vector<IPAddress> v = all();
-  for(unsigned int i = 0; i < v.size(); i++){
-    s += i2s(v[i]);
-    s += "/";
-    s += i2s(ip2group(v[i]));
-    s += " ";
-  }
-  s += "\n";
-  cout << s;
-#endif
+  check(true);
 }
 
-// Make sure we know about exactly the nodes we should know.
+// Do we know the nodes we are supposed to know?
 void
-Kelips::check()
+Kelips::check(bool doprint)
 {
   int gc[_k]; // how many do we know of from each group?
   for(int i = 0; i < _k; i++)
@@ -81,15 +67,30 @@ Kelips::check()
     gc[g] += 1;
   }
 
-  string s;
-  s = i2s(ip());
-  s += ": ";
-  for(int i = 0; i < _k; i++){
-    s += i2s(gc[i]);
-    s += " ";
+  bool stable = true;
+  if(gc[group()] != _k - 1)
+    stable = false;
+  for(int i = 0; i < _k; i++)
+    if(i != group() && gc[i] != 2)
+      stable = false;
+
+  if(_stable == false && stable){
+    _stable = true;
+    cout << now() << " " << ip() << " stable after " << _rounds << " rounds\n";
   }
-  s += "\n";
-  cout << s;
+
+  if(doprint){
+    string s;
+    s = i2s(ip());
+    s += (_stable ? " s" : " u");
+    s += ": ";
+    for(int i = 0; i < _k; i++){
+      s += i2s(gc[i]);
+      s += " ";
+    }
+    s += "\n";
+    cout << s;
+  }
 }
 
 void
@@ -101,7 +102,7 @@ Kelips::join(Args *a)
 
   if(wkn != ip()){
     // Remember well known node.
-    gotinfo(Info(wkn));
+    gotinfo(Info(wkn, now()));
 
     // Tell wkn about us, and ask it for a few random nodes.
     IPAddress myip = ip();
@@ -142,68 +143,60 @@ Kelips::insert(Args *a)
 void
 Kelips::handle_join(IPAddress *caller, vector<Info> *ret)
 {
-  gotinfo(Info(*caller));
-  if(_info.size() > 0){
-    for(int i = 0; i < _gossip_items; i++){
-      IPAddress x = random_peer();
-      (*ret).push_back(*(_info[x]));
-    }
-  }
+  gotinfo(Info(*caller, now())); // XXX caller should supply an Info
+  *ret = gossip_msg(ip2group(*caller));
 }
 
 // This node has just learned about another node.
 // Remember the information, prepare to gossip it.
+// Enforce the invariant that we have at most 2 contacts
+// for each foreign group.
+// XXX should choose closest contacts.
 void
 Kelips::gotinfo(Info i)
 {
-  if(i._ip != ip() && _info.find(i._ip) == _info.end()){
-    cout << ip() << " got " << i._ip << "\n";
-    _info[i._ip] = new Info(i);
+  if(i._ip == ip())
+    return;
+
+  bool create = false;
+
+  if(_info.find(i._ip) == _info.end()){
+    if(ip2group(i._ip) == group()){
+      create = true;
+    } else {
+      vector<IPAddress> l = grouplist(ip2group(i._ip));
+      if(l.size() < 2){
+        create = true;
+      }
+    }
+  } else if (i._heartbeat > _info[i._ip]->_heartbeat){
+    _info[i._ip]->_heartbeat = i._heartbeat;
   }
+
+  if(create){
+    _info[i._ip] = new Info(i);
+    _info[i._ip]->_rounds = _item_rounds;
+  }
+
+  check(false);
 }
 
 // Return the IP address of a random peer chosen
-// from our group members or contacts.
+// from our group members or contacts, depending on mygroup.
+// Might return 0.
 IPAddress
-Kelips::random_peer()
+Kelips::random_peer(bool mygroup)
 {
-  int n = random() % _info.size();
-  map<IPAddress, Info *>::const_iterator i;
-  for(i = _info.begin(); n-- > 0; ++i)
-    ;
-  return i->first;
-}
+  vector<IPAddress> l;
 
-// Send one gossip message to one other node.
-// Chooses what to say and who to say it to.
-// Always include an entry for ourselves.
-// XXX ought to send the most recently learned items.
-// XXX ought to send to nearby nodes preferentially.
-void
-Kelips::gossip(void *junk)
-{
-  if(_info.size() > 0){
-    IPAddress target = random_peer();
-    vector<Info> msg;
-    msg.push_back(Info(ip()));
-    for(int i = 0; i < _gossip_items; i++){
-      IPAddress x = random_peer();
-      msg.push_back(*(_info[x]));
-    }
-    doRPC(target, &Kelips::handle_gossip, &msg, (void *) 0);
-  }
-
-  delaycb(_gossip_interval, &Kelips::gossip, (void *) 0);
-}
-
-void
-Kelips::handle_gossip(vector<Info> *msg, void *ret)
-{
-  unsigned int i;
-
-  for(i = 0; i < msg->size(); i++){
-    gotinfo((*msg)[i]);
-  }
+  if(mygroup)
+    l = grouplist(group());
+  else
+    l = contactlist();
+  if(l.size() > 0)
+    return l[random() % l.size()];
+  else
+    return 0;
 }
 
 // Return a list of all the IP addresses in _info.
@@ -219,30 +212,110 @@ Kelips::all()
   return l;
 }
 
-// Periodically get rid of irrelevant _info entries.
-// Retains only fresh-looking entries from the same
-// group, plus two contacts in each other group.
-// XXX should keep the closest contacts, not random ones.
-// XXX why do this in the background? why not just decide
-//   which contacts to accept in gotinfo()?
-void
-Kelips::purge(void *junk)
+// Return the list of the IP addresses in _info in our group.
+vector<IPAddress>
+Kelips::grouplist(int g)
 {
-  int gc[_k]; // how many do we know of from each group?
-  for(int i = 0; i < _k; i++)
-    gc[i] = 0;
+  vector<IPAddress> l;
+  for(map<IPAddress, Info *>::const_iterator ii = _info.begin();
+      ii != _info.end();
+      ++ii){
+    if(ip2group(ii->second->_ip) == g)
+      l.push_back(ii->first);
+  }
+  return l;
+}
 
-  vector<IPAddress> l = all();
-  for(unsigned int i = 0; i < l.size(); i++){
-    Info *in = _info[l[i]];
-    int g = ip2group(in->_ip);
-    if(g != group() && gc[g] >= _n_contacts){
-      _info.erase(in->_ip);
-      delete in;
-    } else {
-      gc[g] += 1;
+// Return the list of the IP addresses *not* in our group.
+vector<IPAddress>
+Kelips::contactlist()
+{
+  vector<IPAddress> l;
+  for(map<IPAddress, Info *>::const_iterator ii = _info.begin();
+      ii != _info.end();
+      ++ii){
+    if(ip2group(ii->second->_ip) != group())
+      l.push_back(ii->first);
+  }
+  return l;
+}
+
+// Create a gossip message.
+// Always include an entry for ourselves.
+// g allows targeted "pull" gossip during join, critical
+// to avoid disconnection!
+// XXX ought to send half new items, half old items.
+// XXX ought to send newer heartbeats preferentially????
+vector<Kelips::Info>
+Kelips::gossip_msg(int g)
+{
+  vector<Info> msg;
+
+  // Include this node w/ new heartbeat in every gossip.
+  msg.push_back(Info(ip(), now()));
+
+  // Include some nodes from our group.
+  vector<IPAddress> gl = grouplist(g);
+  if(gl.size() > 0){
+    for(int i = 0; i < _group_ration; i++){
+      IPAddress ip = gl[random() % gl.size()];
+      msg.push_back(*_info[ip]);
     }
   }
 
+  // Include some contacts.
+  vector<IPAddress> cl = contactlist();
+  if(cl.size() > 0){
+    for(int i = 0; i < _contact_ration; i++){
+      IPAddress ip = cl[random() % cl.size()];
+      msg.push_back(*_info[ip]);
+    }
+  }
+
+  return msg;
+}
+
+// One round of gossiping.
+// Pick a few items to send, and send them to a few other nodes.
+// XXX ought to send to nearby nodes preferentially (Section 2.1).
+void
+Kelips::gossip(void *junk)
+{
+  vector<Info> msg = gossip_msg(group());
+
+  for(int i = 0; i < _group_targets; i++){
+    IPAddress target = random_peer(true);
+    if(target != 0)
+      doRPC(target, &Kelips::handle_gossip, &msg, (void *) 0);
+  }
+
+  for(int i = 0; i < _contact_targets; i++){
+    IPAddress target = random_peer(false);
+    if(target != 0)
+      doRPC(target, &Kelips::handle_gossip, &msg, (void *) 0);
+  }
+
+  _rounds++;
+
+  delaycb(_round_interval, &Kelips::gossip, (void *) 0);
+}
+
+void
+Kelips::handle_gossip(vector<Info> *msg, void *ret)
+{
+  unsigned int i;
+
+  for(i = 0; i < msg->size(); i++){
+    gotinfo((*msg)[i]);
+  }
+}
+
+// Periodically get rid of irrelevant _info entries.
+// Retains only fresh-looking entries from the same
+// group, plus two contacts in each other group.
+// XXX
+void
+Kelips::purge(void *junk)
+{
   delaycb(_purge_interval, &Kelips::purge, (void *) 0);
 }
--- p2psim/protocols/kelips.h
+++ p2psim/protocols/kelips.h
@@ -4,12 +4,15 @@
 // Differences from Kelips as described in IPTPS 2003 paper:
 // I look up IDs, not files.
 // Not clear who you gossip to across groups, and what you say.
-// On startup, does well-known node maintain lots of information about
-//   other groups? If not, how do 
 // When/how do you learn RTT to a new node? So you know whether to
 //   keep it in your contacts list.
-// Do the gossip messages convey how stale the forwarded info is?
-// How do we decide a node is so stale as to be dead?
+// How often does a node generate a new heartbeat value?
+// When does a node send Info for itself? random? every gossip?
+// For how many rounds do they gossip a new item?
+// Do existing items with new heartbeats count as new for gossip?
+// How big are the rations mentions in 2.1? In the eval.
+// Pull at join and then all push often leaves isolated nodes,
+//   since maybe nobody pushes to me. So what happens at join?
 
 #include "p2psim/p2protocol.h"
 #include "consistenthash.h"
@@ -34,10 +37,25 @@ public:
  private:
   typedef ConsistentHash::CHID ID;
 
-  static const int _gossip_interval = 1000;
-  static const int _gossip_items = 5;
-  static const int _purge_interval = 5000;
+  // send gossip every two seconds, to 3 targets in group,
+  // and three contacts outside the group. Section 4.
+  static const int _round_interval = 2000;
+  static const int _group_targets = 3;
+  static const int _contact_targets = 3;
+
+  // Items per gossip packet. Sections 2 and 4 imply 272/40 = 8 total.
+  // XXX but doesn't specify the group vs contact breakdown.
+  static const int _group_ration = 4;
+  static const int _contact_ration = 2;
+
+  // number of contacts for each foreign group. Section 2.
   static const int _n_contacts = 2;
+
+  // how many times to gossip a new item. XXX not specified in paper.
+  static const int _item_rounds = 4;
+
+  // how often to think about deleting nodes w/ expired heartbeats.
+  static const int _purge_interval = 5000;
 
   int _k; // number of affinity groups, should be sqrt(n)
 
@@ -45,13 +63,18 @@ public:
   class Info {
   public:
     IPAddress _ip;
-    Info(IPAddress ip) { _ip = ip; }
-    Info() { }
+    Time _heartbeat; // when _ip last spoke to anyone.
+    int _rounds;     // how many rounds to send for.
+    Info(IPAddress ip, Time hb) { _ip = ip; _heartbeat = hb; _rounds = 0; }
+    Info() { _ip = 0; _heartbeat = 0; _rounds = 0; }
   };
 
   // Set of nodes that this node knows about.
   // This is the paper's Affinity Group View and Contacts.
   map<IPAddress, Info *> _info;
+
+  int _rounds;  // how many?
+  bool _stable;
 
   void gotinfo(Info i);
   int ip2group(IPAddress xip) { return(ip2id(xip) % _k); }
@@ -61,13 +84,16 @@ public:
   }
   ID id() { return ip2id(ip()); }
   int group() { return ip2group(ip()); }
-  IPAddress random_peer();
+  IPAddress random_peer(bool);
   void gossip(void *);
   void handle_gossip(vector<Info> *, void *);
   void purge(void *);
   vector<IPAddress> all();
-  void check();
+  vector<IPAddress> grouplist(int g);
+  vector<IPAddress> contactlist();
+  void check(bool doprint);
   void handle_join(IPAddress *caller, vector<Info> *ret);
+  vector<Info> gossip_msg(int g);
 };
 
 #endif
