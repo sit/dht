1724105a8fe65d2c391c005ee4755155a1cae8b2
Jinyang Li <jinyang@csail.mit.edu>
2004-06-15 18:56:06 +0000

    OneHop Observer

---
+++ p2psim/observers/onehopobserver.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2003 [Jinyang Li]
+ *                    Massachusetts Institute of Technology
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef __ONEHOP_OBSERVER_H
+#define __ONEHOP_OBSERVER_H
+
+#include "p2psim/observer.h"
+#include "protocols/onehop.h"
+
+class OneHopObserver : public Observer {
+public:
+  OneHopObserver(Args*);
+  ~OneHopObserver();
+  static OneHopObserver* Instance(Args*);
+  vector<Chord::IDMap> get_sorted_nodes();
+  void addnode(Chord::IDMap n) { 
+    vector<Chord::IDMap>::iterator p =
+      upper_bound(ids.begin(),ids.end(),n,Chord::IDMap::cmp);
+    if (p->id!=n.id) {
+      ids.insert(p,1,n);
+    }
+  }
+  void delnode(Chord::IDMap n) { 
+    vector<Chord::IDMap>::iterator p =
+      find(ids.begin(),ids.end(),n);
+    ids.erase(p);
+  }
+
+  virtual void kick(Observed *, ObserverInfo *) {};
+private:
+  static OneHopObserver *_instance;
+  string _type;
+
+  void init_state();
+  vector<Chord::IDMap> ids;
+};
+
+#endif // __CHORD_OBSERVER_H
