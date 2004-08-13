00ff7d704e005e726c98e15092509e39ecbd4569
Athicha Muthitacharoen <athicha@csail.mit.edu>
2004-08-13 21:49:21 +0000

    Make it write output to trace file. Set op intervals right.

--- devel/pk_consistency.C
+++ devel/pk_consistency.C
@@ -3,6 +3,7 @@
 #include "dhashclient.h"
 #include <sfscrypt.h>
 #include <sys/time.h>
+#include "modlogger.h"
 
 //  ip:11978
 
@@ -10,10 +11,14 @@
 #define WRITE 2
 #define READ 3
 #define DATASIZE 8192
+#define trace modlogger ("pkc")
 
 ptr<sfspriv> sk;
+sfs_pubkey2 pk;
+sfs_sig2 s;
 char data[DATASIZE];
 int interval;
+char out[100];
 
 void pk_read (dhashclient, chordID);
 void pk_write_cb (dhashclient, long, dhash_stat, ptr<insert_info>);
@@ -38,14 +43,13 @@ pk_read_cb (dhashclient dhash, dhash_stat stat, ptr<dhash_block> blk,
     timeval tp;
     gettimeofday (&tp, NULL);
     
-    keyhash_payload p;
-    p.decode (blk);
+    ptr<keyhash_payload> p = keyhash_payload::decode (blk);
     
-    warn << tp.tv_sec * (u_int64_t)1000000 << "." << tp.tv_usec << ": "
-	 << "READ key = " << blk->ID << "\n"
-	 << " version = " << p.version ()  << "\n";
+    trace << tp.tv_sec << "." << tp.tv_usec << ": "
+	  << "READ key " << p->id (pk)
+	  << " version " << p->version ()  << "\n";
 
-    timecb (interval, wrap (&pk_read, dhash, blk->ID));
+    delaycb (interval, wrap (&pk_read, dhash, p->id (pk)));
 
   } else {
     warn << "READ error dhash_stat: " << stat << "\n";
@@ -79,11 +83,11 @@ pk_write_cb (dhashclient dhash, long version,
     timeval tp;
     gettimeofday (&tp, NULL);
     
-    warn << tp.tv_sec * (u_int64_t)1000000 << "." << tp.tv_usec << ": "
-	 << "WROTE key = " << i->key << "\n" 
-	 << "  version = " << version << "\n";
+    trace << tp.tv_sec << "." << tp.tv_usec << ": "
+	  << "WROTE key " << i->key 
+	  << "  version " << version << "\n";
 
-    timecb (interval, wrap (&pk_write, dhash, version));
+    delaycb (interval, wrap (&pk_write, dhash, version));
   } else {
     warn << "WRITE error dhash_stat: " << stat << "\n";
     exit (-1);
@@ -94,9 +98,8 @@ void
 pk_write_init_cb (dhashclient dhash, dhash_stat stat, 
 		  ptr<dhash_block> blk, vec<chordID> path)
 {
-  keyhash_payload p;
-  p.decode (blk);
-  keyhash_payload newp (p.version () + 1, str (data, DATASIZE));
+  ptr<keyhash_payload> p = keyhash_payload::decode (blk);
+  keyhash_payload newp (p->version () + 1, str (data, DATASIZE));
   sfs_pubkey2 pk;
   sfs_sig2 s;
   newp.sign (sk, pk, s);
@@ -129,13 +132,13 @@ main (int argc, char **argv)
   int freq = atoi (argv[3]); //writes and reads per minute
   interval = 60/freq; // in seconds
   str key = file2wstr (argv[4]);
-  warn << key << "\n";
   sk = sfscrypt.alloc_priv (key, SFS_SIGN);
 
   keyhash_payload p (0, str (data, DATASIZE));
-  sfs_pubkey2 pk;
-  sfs_sig2 s;
   p.sign (sk, pk, s);
+
+  int logfd = open ("pkc.log", O_WRONLY | O_APPEND | O_CREAT, 0666);
+  modlogger::setlogfd (logfd);
   
   switch (atoi (argv[2])) {
   case NEWBLOCK: {
@@ -149,6 +152,7 @@ main (int argc, char **argv)
     break;
   }
   case READ: {
+    //warn << "Reading bID " << p.id (pk) << "\n"; 
     dhash.retrieve (p.id (pk), DHASH_KEYHASH, wrap (&pk_read_cb, dhash));
     break;
   }
