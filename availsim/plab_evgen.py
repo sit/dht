2a0efe158cc766f4f98938da04bbfcaf3e62072b
Emil Sit <sit@mit.edu>
2004-10-15 00:45:09 +0000

    Avoid duplicate block inserts. Only put in 1000 blocks, and do it after 200 time steps to allow nodes to ramp up and join.

--- availsim/plab_evgen.py
+++ availsim/plab_evgen.py
@@ -17,6 +17,7 @@ from utils import random_id, random_interval, make_chordID
 
 mu_i	= 5
 sd_i	= 5
+maxblocks = 1000
 
 def str2bigint(s):
     a = map(ord, s)
@@ -26,9 +27,15 @@ def str2bigint(s):
 	v = v | d
     return v
 
+inserted = {}
 def random_blockid ():
     s = sha.sha("%d" % random_id ())
     id = str2bigint (s.digest ())
+    while id in inserted:
+	# Damn birthdays.
+	s = sha.sha("%d" % random_id ())
+	id = str2bigint (s.digest ())
+    inserted[id] = 1
     return id
 
 
@@ -46,11 +53,13 @@ for line in fh.readlines ():
     t = t - firsttime
     t /= 60
 
-    if t != lasttime:
-	for t in xrange(lasttime+1,t-1):
-	    ni = random_interval (mu_i, sd_i)
-	    while ni > 0:
-		print t, "insert", nnode, random_blockid ()
-		ni -= 1
+    if t != lasttime and t > 200 :
+	if maxblocks > 0:
+	    for t in xrange(lasttime+1,t-1):
+		ni = random_interval (mu_i, sd_i)
+		while ni > 0 and maxblocks > 0:
+		    print t, "insert", nnode, random_blockid ()
+		    ni -= 1
+		    maxblocks -= 1
 	lasttime = t
     print t, e, make_chordID (ip, 11977, 0)
