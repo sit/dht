11022ff7a2022ed240735d3680972666dbbe9af2
Frank Dabek <fdabek@csail.mit.edu>
2005-02-25 00:49:36 +0000

    lsdping: new utitlity for app level pings

---
+++ devel/lsdping.C
@@ -0,0 +1,89 @@
+#include <async.h>
+#include <dns.h>
+#include <arpc.h>
+#include <aios.h>
+
+#include <chord_types.h>
+#include <chord_prot.h>
+#include <fingers_prot.h>
+#include <merkle_sync_prot.h>
+#include <misc_utils.h>
+#include <id_utils.h>
+#include <merkle_misc.h>
+
+#include "rpclib.h"
+
+void process (int64_t starttime, int seqno, chord_node dst, 
+	      chord_nodelistextres *lst, clnt_stat status);
+void doit (chord_node dst, int seqno);
+
+void
+process (int64_t starttime, int seqno, chord_node dst, 
+	 chord_nodelistextres *lst,
+	 clnt_stat status)
+{
+  if (status) {
+    warn << seqno << ": RPC error\n";
+  } else {
+    warn << seqno << ": from " << dst.r.hostname << ":" << dst.r.port << " " << (getusec() - starttime)/1000 << "msecs \n";
+  }
+
+  delete lst;
+  if (seqno % 10 == 0)
+    delaycb(1, wrap(&doit, dst, seqno + 1));
+}
+
+void doit (chord_node dst, int seqno) {
+  for (int i = 0; i < 10; i++) {
+    int64_t usec = getusec ();
+    ptr<chordID> ga = New refcounted<chordID> (dst.x);
+    chord_nodelistextres *lst = New chord_nodelistextres ();
+    doRPC (dst, chord_program_1, CHORDPROC_GETSUCC_EXT,
+	   ga, lst,
+	   wrap(&process, usec, seqno + i, dst, lst));
+  }
+}
+
+
+int
+main (int argc, char *argv[])
+{
+  int ch;
+  while ((ch = getopt (argc, argv, "d")) != -1)
+    switch (ch) {
+    case 'd':
+      ;
+    default:
+      fatal << "lsdping: -d host port vnodenum\n";
+      break;
+    }
+
+  argc -= optind;
+  argv += optind;
+  
+  if (argc < 3) 
+    fatal << "lsdping: -d host port vnodenum\n";
+
+  chord_node dst;
+
+  if (inet_addr (argv[0]) == INADDR_NONE) {
+    // yep, this still blocks.
+    struct hostent *h = gethostbyname (argv[0]);
+    if (!h)
+      fatal << "Invalid address or hostname: " << argv[0] << "\n";
+    struct in_addr *ptr = (struct in_addr *) h->h_addr;
+    dst.r.hostname = inet_ntoa (*ptr);
+  } else {
+    dst.r.hostname = argv[0];
+  }
+
+  dst.r.port = atoi (argv[1]);
+  dst.vnode_num = atoi (argv[2]);
+  dst.x = make_chordID (dst.r.hostname, dst.r.port, dst.vnode_num);
+
+  doit (dst, 0);
+
+  amain ();
+}
+  
+
