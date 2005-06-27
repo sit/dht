840ac9e8a839983e8dbf502bac8d0cd013ab1477
Frank Dabek <fdabek@csail.mit.edu>
2005-06-27 19:22:33 +0000

    fix linux build (caused by gross duplicate library) with less-gross code copying. untested.

--- merkle/merkle_misc.C
+++ merkle/merkle_misc.C
@@ -1,6 +1,7 @@
 #include "merkle_misc.h"
 #include "id_utils.h"
 #include "dhblock_noauth_srv.h"
+#include "bigint.h"
 
 vec<merkle_hash>
 database_get_keys (dbfe *db, u_int depth, const merkle_hash &prefix)
@@ -34,9 +35,24 @@ to_merkle_key (ptr<dbfe> db, ptr<dbrec> key, dhash_ctype c)
     break;
   case DHASH_NOAUTH:
     {
-      ptr<dbrec> dbkey = dhblock_noauth_srv::get_database_key (dbrec2id(key));
-      ptr<dbrec> data = db->lookup (dbkey);
-      ret = dhblock_noauth_srv::get_merkle_key (dbrec2id(key), data);
+      //ptr<dbrec>dbkey = dhblock_noauth_srv::get_database_key (dbrec2id(key));
+      chordID dbkey = dbrec2id(key) >> 32;
+      dbkey = dbkey << 32;
+
+      ptr<dbrec> data = db->lookup (id2dbrec(dbkey));
+      //      ret = dhblock_noauth_srv::get_merkle_key (dbrec2id(key), data);
+      assert (data);
+      
+      //get the low bits: xor of the marshalled words
+      long *d = (long *)data->value;
+      long hash = 0;
+      for (int i = 0; i < data->len/4; i++) 
+	hash ^= d[i];
+      
+
+      chordID mkey = dbkey | bigint(hash);
+      
+      return id2dbrec(mkey);
     }
     break;
   default:
