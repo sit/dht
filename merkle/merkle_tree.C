bc44cc5f6ed24e985282d5695bef510f2d91f4fd
Emil Sit <sit@mit.edu>
2007-01-19 10:54:35 -0500

    Comment out verbose message in merkle_tree constructor.

--- merkle/merkle_tree.C
+++ merkle/merkle_tree.C
@@ -34,7 +34,7 @@ merkle_tree::merkle_tree () :
   do_rehash (true)
 {
   root = New merkle_node();
-  warn << "root: " << root->isleaf() << "\n";
+  // warn << "root: " << root->isleaf() << "\n";
 }
 
 merkle_tree::~merkle_tree ()
