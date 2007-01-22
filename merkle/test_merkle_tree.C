38272cd8b14907d03eda69911df8627aa5e1ae0d
Emil Sit <sit@mit.edu>
2007-01-22 14:26:15 -0500

    Rewrite test_merkle_tree, folding in some disk tests.
    Test for success of inserts, reads, iteration, removes,
    inserts-after-remove, reads-after-remove.
    
    Also write some basic disk-only specific tests that try to achieve
    some parallelism in reading/writing disk-based Merkle trees.

--- merkle/test_merkle_tree.C
+++ merkle/test_merkle_tree.C
@@ -1,13 +1,50 @@
-#include "merkle.h"
-#include "misc_utils.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/*
- * Basic basic timing tests of Merkle tree
- * Should probably test removal as well.
- */
+#include <qhash.h>
+#include "merkle.h"
+#include "merkle_tree_disk.h"
+#include <misc_utils.h>
+#include <id_utils.h>
+
+typedef bhash<chordID, hashID> keys_t;
+
+static char *indexpath = "/tmp/index.mrk";
+static char *internalpath = "/tmp/internal.mrk";
+static char *leafpath  = "/tmp/leaf.mrk";
 
 void
-insert_blocks (merkle_tree *mtree, int upto, bool random)
+cleanup ()
+{
+  unlink (indexpath);
+  unlink (internalpath);
+  unlink (leafpath);
+}
+
+bool
+reap (int pid, bool wait = true)
+{
+  int wpid, status, options = 0;
+
+  if (!wait)
+    options = WNOHANG;
+  wpid = waitpid (pid, &status, options);
+  if (wpid < 0) {
+    perror("waitpid");
+    exit (1);
+  }
+  if (!wait && (wpid == 0))
+    return false;
+  if (wpid != pid)
+    fatal ("unexpected pid reaped: %d\n", wpid);
+  if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
+    fatal << "child exited unhappily\n";
+  return true;
+}
+
+void
+insert_blocks (merkle_tree *mtree, int upto, bool random,
+    keys_t *keys = NULL)
 {
   for (int i = 1; i <= upto; i++) {
     merkle_hash key;
@@ -16,60 +53,252 @@ insert_blocks (merkle_tree *mtree, int upto, bool random)
     else
       key = i;
 
+    if (keys)
+      keys->insert (tobigint (key));
     mtree->insert (key);
-    //mtree->dump ();
-
-    if (i % 10000 == 0) {
-      warn << "inserted " << i << " blocks..\n";
-      //mtree->dump ();
-      //dumpdb ();
-    }
   }
 }
 
 void
-test (int upto, bool rand = true)
+test_numkeys (merkle_tree *tree, unsigned int cnt)
 {
-  warn << "\n=================== test_" << (rand ? "rand" : "incr")
-       << " upto " << upto << "\n";
+  merkle_node *root = tree->get_root ();
+  assert (root->count == cnt);
+  tree->lookup_release (root);
+}
 
+void
+test_insertions (str msg,
+    merkle_tree *mtree, uint nkeys, bool rand, keys_t &keys)
+{
+  uint okeyssz = keys.size ();
+  warn << msg << " bulk insertion... ";
   u_int64_t start = getusec ();
-  merkle_tree mtree;
-  mtree.set_rehash_on_modification (false);
-  insert_blocks (&mtree, upto, rand);
-  warn << "completed in: " << (getusec ()-start)/1000 << "ms\n";
+  mtree->set_rehash_on_modification (false);
+  insert_blocks (mtree, nkeys, rand, &keys);
+  warn << "completed in: " << (getusec ()-start)/1000 << "ms... ";
+  assert ((keys.size () - okeyssz) == nkeys);
+  warn << "OK\n";
+
+  warn << msg << " hash full tree... ";
   start = getusec ();
-  mtree.hash_tree ();
-  warn << "rehash completed in: " << (getusec ()-start)/1000 << "ms\n";
-  mtree.check_invariants ();
-  // mtree.dump ();
+  mtree->hash_tree ();
+  warn << "completed in: " << (getusec ()-start)/1000 << "ms... ";
+  test_numkeys (mtree, keys.size ());
+  warn << "OK\n";
+
+  warn << msg << " check invariants... ";
+  mtree->check_invariants ();
+  warn << "OK.\n";
+
+  mtree->set_rehash_on_modification (true);
+}
+
+const vec<chordID> &
+test_reads (merkle_tree *mtree, const keys_t &keys)
+{
+  static vec<chordID> intree;
+  intree.clear ();
+  unsigned int nkeys = keys.size ();
+
+  warn << "All key read (get_keyrange)... ";
+  intree = mtree->get_keyrange (
+      0, (chordID (1) << 160) - 1, nkeys + 1);
+  assert (intree.size () == nkeys);
+  for (uint i = 0; i < intree.size (); i++) {
+    assert (keys[intree[i]]);
+  }
+  warn << "OK\n";
+
+  warn << "Randomized key lookups... ";
+  uint limit = 128 + int(nkeys * 0.2);
+  if (limit > nkeys)
+    limit = nkeys;
+  uint order[nkeys];
+  for (uint i=0; i<nkeys; i++) order[i] = i;
+  for (uint i = 0; i < limit; i++) {
+    uint j = i + random() % (nkeys-i);
+    uint key = order[j]; order[j] = order[i]; order[i] = key;
+    assert (mtree->key_exists (intree[key]));
+    merkle_node *n = mtree->lookup (to_merkle_hash (intree[key]));
+    assert (n);
+    assert (n->isleaf ());
+  }
+  warn << "OK\n";
+
+  warn << "Missing key lookups... ";
+  int todo = limit;
+  while (todo) {
+    chordID c = make_randomID ();
+    if (keys[c])
+      continue;
+    todo--;
+    assert (!mtree->key_exists (c));
+    merkle_node *n = mtree->lookup (to_merkle_hash (c));
+    assert (n);
+  }
+  warn << "OK\n";
+
+  warn << "Random range reads... ";
+  chordID highest = (((chordID) 1) << 160) - 1;
+  for (int i = 0; i < 32; i++) {
+    chordID min = make_randomID ();
+    chordID max = min + make_randomID () % (highest - min);
+    //warn << "  get_keyrange (" << min << ", " << max << ", " << nkeys << ")\n";
+    vec<chordID> rangekeys = mtree->get_keyrange (min, max, nkeys);
+
+    keys_t subset;
+    for (uint j = 0; j < nkeys; j++)
+      if (betweenbothincl (min, max, intree[j]))
+	subset.insert (intree[j]);
+    assert (subset.size () == rangekeys.size ());
+    for (uint j = 0; j < rangekeys.size (); j++)
+      assert (subset[rangekeys[j]]);
+  }
+  warn << "OK\n";
+  return intree;
+}
+
+void
+test (str msg, merkle_tree *mtree, uint nkeys, bool rand = true)
+{
+  warn << "\n=================== " << msg 
+       << " test_" << (rand ? "rand" : "incr")
+       << " nkeys " << nkeys << "\n";
+
+  keys_t keys;
+  keys.clear ();
+
+  test_insertions ("Initial", mtree, nkeys, rand, keys);
+  const vec<chordID> intree = test_reads (mtree, keys);
+
+  warn << "Removes... ";
+  uint limit = (nkeys < 128) ? nkeys : (128 + int (nkeys * 0.2));
+  uint order[nkeys];
+  for (uint i=0; i<nkeys; i++) order[i] = i;
+  for (uint i = 0; i < limit; i++) {
+    // warnx << "  " << i+1 << "/" << limit << ".\n";
+    uint j = i + random() % (nkeys-i);
+    uint key = order[j]; order[j] = order[i]; order[i] = key;
+    mtree->remove (intree[key]);
+    keys.remove (intree[key]);
+    test_numkeys (mtree, nkeys - (i + 1));
+    assert (keys.size () == (nkeys - (i + 1)));
+  }
+  warn << "OK\n";
+
+  warn << "Post-remove tree contents check... ";
+  for (uint i = 0; i < nkeys; i++) {
+    if (i < limit)
+      assert (!mtree->key_exists (intree[order[i]]));
+    else
+      assert (mtree->key_exists (intree[order[i]]));
+  }
+  mtree->check_invariants ();
+  warn << "OK\n";
+
+  if (rand)
+    test_insertions ("Post-remove", mtree, 2*limit, rand, keys);
+  test_reads (mtree, keys);
+}
+
+void
+test_merkle_disk_specific ()
+{
+  warn << "\n=================== Merkle Disk Specific Tests\n";
+  merkle_tree *mtree = 
+      New merkle_tree_disk (indexpath, internalpath, leafpath, true);
+
+  keys_t keys;
+  keys.clear ();
+
+  test_insertions ("Initial", mtree, 256, true, keys);
+  delete mtree;
+
+  warn << "Static child read... \n";
+  int pid = fork ();
+  if (pid < 0)
+    fatal ("fork: %m\n");
+  if (pid == 0) {
+    // Child
+    mtree = 
+      New merkle_tree_disk (indexpath, internalpath, leafpath, false);
+
+    const vec<chordID> intree = test_reads (mtree, keys);
+    exit (0);
+  }
+  reap (pid);
+  warn << "Static child read OK\n";
+
+  warn << "Dynamic child reads... \n";
+  vec<chordID> tobeinserted;
+  for (int i = 0; i < 3200; i++)
+    tobeinserted.push_back (make_randomID ());
+  pid = fork ();
+  if (pid > 0) {
+    // Parent
+    bool reaped = false;
+    mtree = 
+      New merkle_tree_disk (indexpath, internalpath, leafpath, true);
+    for (int i = 0; i < 32; i++) {
+      warn << "  Parent insertion burst " << i+1 << "\n";
+      for (int j = 0; j < 100; j++) {
+	mtree->insert (tobeinserted[100*i + j]);
+	keys.insert (tobeinserted[100*i + j]);
+      }
+      if (!reaped)
+	reaped = reap (pid, false);
+      sleep (1);
+    }
+    if (!reaped)
+      reap (pid);
+  } else {
+    chordID highest = (((chordID) 1) << 160) - 1;
+    mtree = 
+      New merkle_tree_disk (indexpath, internalpath, leafpath, false);
+    for (int i = 0; i < 32; i++) {
+      warn << "  Child read-range " << i+1 << "\n";
+      chordID min = make_randomID ();
+      chordID max = min + make_randomID () % (highest - min);
+      vec<chordID> rangekeys = mtree->get_keyrange (min, max, 10000);
+      for (uint j = 0; j < rangekeys.size (); j++)
+	assert (betweenbothincl (min, max, rangekeys[j]));
+      mtree->check_invariants ();
+      sleep (1);
+    }
+    exit (0);
+  } 
+  warn << "Dynamic child reads seem OK\n";
 }
 
 int
-main ()
+main (int argc, char *argv[])
 {
-  // XXX call random_init () ???
-  //     -- between tests...???
-  do {
-    test (1, false);
-    test (64, false);
-    test (65, false);
-    test (64 * 64, false);
-    test (64 * 64 + 1, false);
-    test (10000, false);
-//    test_rand (100000);
-//    test_rand (1000000);
-  } while (0);
+  // Make sure no confusion from previous crashes.
+  cleanup ();
 
-  do {
-    test (1);
-    test (64);
-    test (65);
-    test (64 * 64);
-    test (64 * 64 + 1);
-    test (10000);
-//    test_rand (100000);
-//    test_rand (1000000);
-  } while (0);
+  // Any arguments mean we skip the "normal" tests.
+  if (argc == 1) {
+    int sz[] = { 1, 64, 256, 64*64+1, 10000 };
+    for (uint i = 0; i < sizeof (sz) / sizeof (sz[0]); i++) {
+      merkle_tree *t = New merkle_tree ();
+      test ("In-memory", t, sz[i], false);
+      delete t; t = NULL;
+
+      t = New merkle_tree ();
+      test ("In-memory", t, sz[i], true);
+      delete t; t = NULL;
+    }
+
+    for (uint i = 0; i < sizeof (sz) / sizeof (sz[0]); i++) {
+      merkle_tree *t = 
+	New merkle_tree_disk (indexpath, internalpath, leafpath, true);
+      test ("Disk", t, sz[i]);
+      delete t; t = NULL;
+      cleanup ();
+    }
+  }
+
+  test_merkle_disk_specific ();
+  cleanup ();
 }
-
