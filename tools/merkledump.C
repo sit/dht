5f20d9e3287e3bb022ef41a35a2f616873f9e4fa
Emil Sit <sit@mit.edu>
2007-08-17 14:41:18 -0400

    Allow check_invariants when running merkledump.
    ...via -c option.

--- tools/merkledump.C
+++ tools/merkledump.C
@@ -9,19 +9,29 @@ main (int argc, char *argv[])
 {
   setprogname (argv[0]);
   if (argc < 2)
-    fatal <<  "Usage: " << progname << " merkletreebdb\n";
+    fatal <<  "Usage: " << progname << " [-c] merkletreebdb\n";
+  
+  bool check = false;
+  char *path = argv[1];
+  if (!strcmp (path, "-c")) {
+    path = argv[2];
+    check = true;
+  }
 
   ptr<merkle_tree_bdb> tree = NULL;
-  if (merkle_tree_bdb::tree_exists (argv[1])) {
+  if (merkle_tree_bdb::tree_exists (path)) {
     // Handle case where path is given directly
-    tree = New refcounted<merkle_tree_bdb> (argv[1], true, true);
+    tree = New refcounted<merkle_tree_bdb> (path, true, true);
   } else {
     // Handle case where path may owned by adbd.
     char pathbuf[PATH_MAX];
-    sprintf (pathbuf, "%s/mtree", argv[1]);
+    sprintf (pathbuf, "%s/mtree", path);
     tree = New refcounted<merkle_tree_bdb> (pathbuf, true, true);
     // Fatal if this tree does not exist.
   }
+
+  if (check)
+    tree->check_invariants ();
 
   chordID lastread = 0;
   chordID maxid = (chordID (1) << 160) - 1;
